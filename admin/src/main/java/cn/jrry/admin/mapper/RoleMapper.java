c3171e18eb87de7bb4fca88394f1e4934da0662e
jrchens <jrchens@126.com>
2018-01-16 13:56:32 +0800

    add role

---
+++ admin/src/main/java/cn/jrry/admin/mapper/RoleMapper.java
@@ -0,0 +1,21 @@
+package cn.jrry.admin.mapper;
+
+import cn.jrry.admin.domain.Role;
+import java.util.List;
+import java.util.Map;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Role record);
+
+    Role selectByPrimaryKey(Long id);
+
+    List<Role> selectAll();
+
+    int updateByPrimaryKey(Role record);
+    int removeByPrimaryKey(Role record);
+
+    int count(Map<String,Object> record);
+    List<Role> select(Map<String,Object> record);
+}
