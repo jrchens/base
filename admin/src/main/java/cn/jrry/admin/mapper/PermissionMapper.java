bbf1438160a06a5a4341a8fcb3beb19769ac6a4e
jrchens <jrchens@126.com>
2018-01-16 15:09:20 +0800

    add permission

---
+++ admin/src/main/java/cn/jrry/admin/mapper/PermissionMapper.java
@@ -0,0 +1,22 @@
+package cn.jrry.admin.mapper;
+
+import cn.jrry.admin.domain.Permission;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PermissionMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Permission record);
+
+    Permission selectByPrimaryKey(Long id);
+
+    List<Permission> selectAll();
+
+    int updateByPrimaryKey(Permission record);
+
+    int removeByPrimaryKey(Permission record);
+    int count(Map<String, Object> record);
+    List<Permission> select(Map<String, Object> record);
+}
---
+++ admin/src/main/java/cn/jrry/admin/service/PermissionService.java
@@ -0,0 +1,24 @@
+package cn.jrry.admin.service;
+
+import cn.jrry.admin.domain.Permission;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PermissionService {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Permission record);
+
+    Permission selectByPrimaryKey(Long id);
+
+    List<Permission> selectAll();
+
+    int updateByPrimaryKey(Permission record);
+
+    int removeByPrimaryKey(Permission record);
+
+    int count(Map<String, Object> record);
+
+    List<Permission> select(Map<String, Object> record);
+}
